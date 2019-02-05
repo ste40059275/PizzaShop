0f418f380d9e3b6e8bab40f31deae455e409ad3e
Tyler Stephen Stewart (STE40059275) <STE40059275@serc.ac.uk>
2019-02-05 14:31:24 +0000

    frmPizza

--- PizzaShop/frmPizza.Designer.cs
+++ PizzaShop/frmPizza.Designer.cs
@@ -28,12 +28,243 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnUndo = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnTotal = new System.Windows.Forms.Button();
+            this.btnInvoice = new System.Windows.Forms.Button();
+            this.lblSubtotalD = new System.Windows.Forms.Label();
+            this.lblDiscountD = new System.Windows.Forms.Label();
+            this.lblTotalD = new System.Windows.Forms.Label();
+            this.chkRegular = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.lblPizza = new System.Windows.Forms.Label();
+            this.lstOrder = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnUndo
+            // 
+            this.btnUndo.Location = new System.Drawing.Point(687, 100);
+            this.btnUndo.Name = "btnUndo";
+            this.btnUndo.Size = new System.Drawing.Size(75, 23);
+            this.btnUndo.TabIndex = 0;
+            this.btnUndo.Text = "Undo";
+            this.btnUndo.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(687, 144);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnTotal
+            // 
+            this.btnTotal.Location = new System.Drawing.Point(687, 187);
+            this.btnTotal.Name = "btnTotal";
+            this.btnTotal.Size = new System.Drawing.Size(75, 23);
+            this.btnTotal.TabIndex = 2;
+            this.btnTotal.Text = "Total";
+            this.btnTotal.UseVisualStyleBackColor = true;
+            // 
+            // btnInvoice
+            // 
+            this.btnInvoice.Location = new System.Drawing.Point(687, 330);
+            this.btnInvoice.Name = "btnInvoice";
+            this.btnInvoice.Size = new System.Drawing.Size(75, 23);
+            this.btnInvoice.TabIndex = 3;
+            this.btnInvoice.Text = "Invoice";
+            this.btnInvoice.UseVisualStyleBackColor = true;
+            // 
+            // lblSubtotalD
+            // 
+            this.lblSubtotalD.AutoSize = true;
+            this.lblSubtotalD.Location = new System.Drawing.Point(684, 232);
+            this.lblSubtotalD.Name = "lblSubtotalD";
+            this.lblSubtotalD.Size = new System.Drawing.Size(46, 13);
+            this.lblSubtotalD.TabIndex = 4;
+            this.lblSubtotalD.Text = "Subtotal";
+            // 
+            // lblDiscountD
+            // 
+            this.lblDiscountD.AutoSize = true;
+            this.lblDiscountD.Location = new System.Drawing.Point(684, 259);
+            this.lblDiscountD.Name = "lblDiscountD";
+            this.lblDiscountD.Size = new System.Drawing.Size(49, 13);
+            this.lblDiscountD.TabIndex = 5;
+            this.lblDiscountD.Text = "Discount";
+            // 
+            // lblTotalD
+            // 
+            this.lblTotalD.AutoSize = true;
+            this.lblTotalD.Location = new System.Drawing.Point(684, 288);
+            this.lblTotalD.Name = "lblTotalD";
+            this.lblTotalD.Size = new System.Drawing.Size(31, 13);
+            this.lblTotalD.TabIndex = 6;
+            this.lblTotalD.Text = "Total";
+            // 
+            // chkRegular
+            // 
+            this.chkRegular.AutoSize = true;
+            this.chkRegular.Location = new System.Drawing.Point(541, 335);
+            this.chkRegular.Name = "chkRegular";
+            this.chkRegular.Size = new System.Drawing.Size(15, 14);
+            this.chkRegular.TabIndex = 7;
+            this.chkRegular.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(444, 335);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Regular Customer";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox6);
+            this.groupBox1.Controls.Add(this.pictureBox5);
+            this.groupBox1.Controls.Add(this.pictureBox4);
+            this.groupBox1.Controls.Add(this.pictureBox3);
+            this.groupBox1.Controls.Add(this.pictureBox2);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(47, 51);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(227, 210);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Choose Your Pizza";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(6, 32);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(112, 32);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(6, 93);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(112, 93);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(6, 149);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(112, 149);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            // 
+            // lblPizza
+            // 
+            this.lblPizza.AutoSize = true;
+            this.lblPizza.Location = new System.Drawing.Point(383, 51);
+            this.lblPizza.Name = "lblPizza";
+            this.lblPizza.Size = new System.Drawing.Size(0, 13);
+            this.lblPizza.TabIndex = 10;
+            // 
+            // lstOrder
+            // 
+            this.lstOrder.FormattingEnabled = true;
+            this.lstOrder.Location = new System.Drawing.Point(386, 99);
+            this.lstOrder.Name = "lstOrder";
+            this.lstOrder.Size = new System.Drawing.Size(120, 95);
+            this.lstOrder.TabIndex = 11;
+            // 
+            // frmPizza
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstOrder);
+            this.Controls.Add(this.lblPizza);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chkRegular);
+            this.Controls.Add(this.lblTotalD);
+            this.Controls.Add(this.lblDiscountD);
+            this.Controls.Add(this.lblSubtotalD);
+            this.Controls.Add(this.btnInvoice);
+            this.Controls.Add(this.btnTotal);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnUndo);
+            this.Name = "frmPizza";
             this.Text = "frmPizza";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnUndo;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnTotal;
+        private System.Windows.Forms.Button btnInvoice;
+        private System.Windows.Forms.Label lblSubtotalD;
+        private System.Windows.Forms.Label lblDiscountD;
+        private System.Windows.Forms.Label lblTotalD;
+        private System.Windows.Forms.CheckBox chkRegular;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblPizza;
+        private System.Windows.Forms.ListBox lstOrder;
     }
 }
--- PizzaShop/frmPizza.cs
+++ PizzaShop/frmPizza.cs
@@ -16,5 +16,211 @@ namespace PizzaShop
         {
             InitializeComponent();
         }
+
+
+
+
+
+        //Variables to hold the values 
+        public static double dPrice, dSubTotal, dDiscount = 0;
+        public static int dTotal = 0;
+
+        private void pichawiian_MouseLeave(object sender, EventArgs e)
+        {
+            //code to clear the pizza name and price display.
+            lblPizza.Text = "";
+        }
+
+        private void pichawiian_click(object sender, EventArgs e)
+        {
+            //code to add the pizza name and price to the list box and variable
+            lstOrder.Items.Add("Hawaiian £8.99");
+            dPrice = 8.99;
+
+            dSubTotal = dSubTotal + dPrice;
+        }
+
+        private void picFourCheese_Click(object sender, EventArgs e)
+        {
+            //code to add the pizza name and price to the list box
+            lstOrder.Items.Add("Four Cheese £7.99");
+            dPrice = 7.99;
+
+
+            dSubTotal = dSubTotal + dPrice;
+        }
+
+        private void picVeg_click(object sender, EventArgs e)
+        {
+            //code to add the pizza name and price to the list box
+            lstOrder.Items.Add("Vegetarian £6.49");
+            dPrice = 6.49;
+
+
+            dSubTotal = dSubTotal + dPrice;
+        }
+
+        private void picMeatFeast_Click(object sender, EventArgs e)
+        {
+            //code to add the pizza name and price to the list box
+            lstOrder.Items.Add("Meat Feast £8.99");
+            dPrice = 8.99;
+
+
+            dSubTotal = dSubTotal + dPrice;
+        }
+
+        private void picPepperoni_Click(object sender, EventArgs e)
+        {
+            //code to add the pizza name and price to the list box
+            lstOrder.Items.Add("Pepperoni £6.99");
+            dPrice = 6.99;
+
+
+            dSubTotal = dSubTotal + dPrice;
+        }
+
+
+        private void picMargareta_Click(object sender, EventArgs e)
+        {
+            //code to add the pizza name and price to the list box
+            lstOrder.Items.Add("Margareta £6.49");
+            dPrice = 6.49;
+
+
+            dSubTotal = dSubTotal + dPrice;
+        }
+
+
+        private void picFourcheese_MouseHover(object sender, EventArgs e)
+        {
+            //Code to display price of pizza
+            lblPizza.Text = "Four Cheese £7.99";
+
+        }
+
+        private void picVeg_MouseLeave(object sender, EventArgs e)
+        {
+            //Code to display price of pizza
+            lblPizza.Text = "Vegetarian £6.49";
+
+        }
+
+        private void picMeatFeast_MouseLeave(object sender, EventArgs e)
+        {
+            //Code to display price of pizza
+            lblPizza.Text = "Meat Feast £8.99";
+
+        }
+
+
+        private void picPepperoni_MouseHover(object sender, EventArgs e)
+        {
+            //Code to display price of pizza
+            lblPizza.Text = "Pepperoni £6.99";
+
+        }
+
+        private void picMargareta_MouseHover(object sender, EventArgs e)
+        {
+            //Code to display price of pizza
+            lblPizza.Text = "Margareta £6.49";
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            //code to reset the order form
+
+            lblDiscountD.Text = "";
+
+            chkRegular.Checked = true;
+            lblSubtotalD.Text = "";
+            lblTotalD.Text = "";
+
+            dSubTotal = 0;
+            dTotal = 0;
+        }
+
+        private void btnTotal_Click(object sender, EventArgs e)
+        {
+            //code to calculate the total and discount if the total is >£20 or customer is regular
+            lblSubtotalD.Text = dSubTotal.ToString("#0.00");
+
+            if (dSubTotal >= 20.0 || chkRegular.Checked == true)
+            {
+                dDiscount = dSubTotal / 100 * 50;
+                dTotal = Convert.ToInt32(dSubTotal - dDiscount);
+
+
+            }
+            else
+            {
+                dDiscount = 0;
+            }
+
+            dTotal = Convert.ToInt32(dSubTotal - dDiscount);
+            lblDiscountD.Text = dDiscount.ToString("#0.00");
+            lblTotalD.Text = dTotal.ToString("#0.00");
+
+
+        }
+
+        private void btnUndo_click(object sender, EventArgs e)
+        {
+            //code to remove the last item from the list
+            lstOrder.Items.RemoveAt(lstOrder.Items.Count - 1);
+            dSubTotal = dSubTotal - dPrice;
+            lblDiscountD.Text = "";
+            lblSubtotalD.Text = "";
+            lblTotalD.Text = "";
+            btnUndo.Enabled = false;
+
+        }
+
+        private void Invoice()
+        {
+            //local variable to store result
+            DialogResult dr;
+            //accept response 
+            dr = MessageBox.Show("Are you sure you want to Proceed?",
+                "Confirm Order",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            //if yes close program
+            if (dr == DialogResult.No)
+            {
+                //Find frmInvoice
+                frmInvoice sw = new frmInvoice();
+
+                //hide frmpizza
+                this.Hide();
+
+                //show frminvoice
+                sw.show();
+            }
+        }
+
+        private void btnInvoice_Click(object sender, EventArgs e)
+        {
+            Invoice();
+        }
+
+        private void pichawiian_MouseHover_1(object sender, EventArgs e)
+        {
+            //code to display price of pizza
+            lblPizza.Text = "Hawaiian £8.99";
+
+        }
+
+
+
+
+
+
+
+
+
     }
+
+
 }
